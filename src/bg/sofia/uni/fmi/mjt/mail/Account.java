2a8eceaad92455d85faa05f38b648a6f4e8a2438
ralisimova <104558937+ralisimova@users.noreply.github.com>
2022-12-08 01:40:18 +0200

    Delete src directory

--- src/bg/sofia/uni/fmi/mjt/mail/Account.java
+++
@@ -1,4 +0,0 @@
-package bg.sofia.uni.fmi.mjt.mail;
-
-public record Account(String emailAddress, String name) {
-}
--- src/bg/sofia/uni/fmi/mjt/mail/Rule.java
+++
@@ -1,5 +0,0 @@
-package bg.sofia.uni.fmi.mjt.mail;
-
-public record Rule(String definition, int priority,String path) {
-
-}
